10c68ff76be4f4abde1384fee519b873b59496d4
Javen Zamojcin <javen.william@gmail.com>
2020-09-22 13:51:39 -0400

    + Matrix Add()

--- src/MatrixUtility.java
+++ src/MatrixUtility.java
@@ -10,6 +10,16 @@ public class MatrixUtility {
         return null;
     }
 
+    public static int[] add(int[] a, int[] b)
+    {
+        int[] result = new int[a.length];
+        for (int i = 0; i < a.length; i++)
+        {
+            result[i] = a[i] ^ b[i];
+        }
+        return result;
+    }
+
     public static void printMatrix(int[][] matrix)
     {
 
