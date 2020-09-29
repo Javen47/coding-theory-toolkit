2ec8589a403e39e950ff9e52f612839b6e2be90d
Javen Zamojcin <javen.william@gmail.com>
2020-09-29 15:26:40 -0400

    Homework 5 Partial Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,293 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException
     {
         Main main = new Main();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner reader = new Scanner(System.in);
 
         main.printOptions();
-        int selectedOption = reader.read();
+        main.promptInputOptions(reader);
+    }
+
+    private void promptInputOptions(Scanner reader) throws IOException
+    {
+        Main main = new Main();
+        int selectedOption = reader.nextInt();
+
         switch (selectedOption)
         {
             case 1:
+                optionOne(reader);
                 break;
             case 2:
-                System.out.println("For the Parity Check Matrix: \n");
-                int[][] PCM = main.promptInputMatrix(reader);
-                System.out.println("For the received vector: \n");
-                int[][] vector = main.promptInputMatrix(reader);
-                CodeUtility.determineOriginalCodeword(PCM, vector);
+                optionTwo(reader);
                 break;
             case 3:
+                optionThree(reader);
                 break;
             case 4:
+                optionFour(reader);
                 break;
             case 5:
+                optionFive(reader);
                 break;
             case 6:
+                optionSix(reader);
                 break;
             case 7:
+                optionSeven(reader);
                 break;
             case 8:
+                optionEight(reader);
+                break;
+            case 9:
+                optionNine(reader);
+                break;
+            case 10:
+                optionTen(reader);
+                break;
+            case 11:
+                optionEleven(reader);
                 break;
             default:
                 main.printOptions();
+                main.promptInputOptions(reader);
                 break;
         }
-
-        //System.out.println("\nResult: ");
     }
 
     private void printOptions()
     {
-        System.out.println("What would you like to do?\n-\n");
-        System.out.println("1. Calculate probability of decoding received codeword correctly");
+        System.out.println("\n\nWhat would you like to do?\n-");
+
+        System.out.println("1. Calculate the probability of decoding a received hamming codeword correctly");
         System.out.println("2. Given a PCM and a received vector, determine original codeword");
+        System.out.println("3. Determine if the given parameters [n, k, d] are the parameters of an MDS code");
+        System.out.println("4. Determine if the given parameters [n, k, d]q imply the existence of a code by satisfying the VG bound");
+        System.out.println("5. Determine if the given parameters [n, k, d] correspond to a given packing radius");
+        System.out.println("6. Calculate the covering radius of a binary PCM");
+        System.out.println("7. Generate the cosets for a PCM");
+        System.out.println("8. Calculate combination");
+        System.out.println("9. Determine if the given parameters [n, k, d]q violate the SP bound");
+        System.out.println("10. Determine if a given vector can be the coset leader of a PCM");
+        System.out.println("11. Calculate the probability of correctly syndrome decoding a received codeword");
     }
 
-    private int[][] promptInputMatrix(BufferedReader reader) throws IOException
+    private void optionOne(Scanner reader)
+    {
+        System.out.println("What is the length of the codeword?");
+        int length = reader.nextInt();
+
+        System.out.println("What is the single bit error probability?");
+        double p = reader.nextDouble();
+
+        double result = CodeUtility.calculateProbabilityOfCorrectHammingCodeDecoding(length, p);
+        System.out.println("Result: " + result);
+    }
+
+    private void optionTwo(Scanner reader) throws IOException
+    {
+        System.out.println("For the Parity Check Matrix:");
+        int[][] PCM = promptInputMatrix(reader);
+        System.out.println("For the received vector:");
+        int[][] vector = promptInputMatrix(reader);
+        MatrixUtility.printMatrix(CodeUtility.determineOriginalCodeword(PCM, vector));
+    }
+
+    private void optionThree(Scanner reader) throws IOException
+    {
+        int[][] parameters = promptInputNKDParameters(reader);
+        for (int[] i : parameters)
+        {
+            if (CodeUtility.determineIfParametersAreOfMDSCode(i[0], i[1], i[2]))
+            {
+                System.out.format("\n[%d, %d, %d] are the parameters of an MDS code", i[0], i[1], i[2]);
+            }
+        }
+    }
+
+    private void optionFour(Scanner reader) throws IOException
+    {
+        int[][] parameters = promptInputNKDQParameters(reader);
+        for (int[] i : parameters)
+        {
+            if (CodeUtility.determineIfCodeExistsWithinVGBound(i[0], i[1], i[2], i[3]))
+            {
+                System.out.format("\n[%d, %d, %d]%d implies the existence of a code", i[0], i[1], i[2], i[3]);
+            }
+        }
+    }
+
+    private void optionFive(Scanner reader) throws IOException
+    {
+        System.out.println("What is the packing radius?");
+        int packingRadius = reader.nextInt();
+
+        int[][] parameters = promptInputNKDParameters(reader);
+        for (int[] i : parameters)
+        {
+            if (CodeUtility.calculatePackingRadius(i[2]) == packingRadius)
+            {
+                System.out.format("\n[%d, %d, %d] correspond to the packing radius", i[0], i[1], i[2]);
+            }
+        }
+    }
+
+    private void optionSix(Scanner reader) throws IOException
+    {
+        System.out.println("For the Parity Check Matrix: ");
+        int[][] PCM = promptInputMatrix(reader);
+        int coveringRadius = CodeUtility.calculateCoveringRadius(PCM);
+        System.out.println("The covering radius is: " + coveringRadius);
+    }
+
+    private void optionSeven(Scanner reader) throws IOException
+    {
+        System.out.println("For the Parity Check Matrix: ");
+        int[][] PCM = promptInputMatrix(reader);
+        for (int[] cosetRep : CodeUtility.cosetReps)
+        {
+            MatrixUtility.printVector(cosetRep);
+            System.out.println("---");
+            int[][] coset = CodeUtility.generateCoset(PCM, cosetRep);
+            MatrixUtility.printMatrix(coset);
+            MatrixUtility.printVector(CodeUtility.findCosetLeader(coset));
+            System.out.println(" - Leader");
+        }
+    }
+
+    private void optionEight(Scanner reader)
+    {
+        System.out.println("What is your n?");
+        int n = reader.nextInt();
+
+        System.out.println("What is your r?");
+        int r = reader.nextInt();
+
+        long result = CodeUtility.combination(n, r);
+        System.out.println("Result: " + result);
+    }
+
+    private void optionNine(Scanner reader) throws IOException
+    {
+        int[][] parameters = promptInputNKDQParameters(reader);
+        for (int[] i : parameters)
+        {
+            if (!CodeUtility.determineIfCodeExistsWithinSPBound(i[0], i[1], i[2], i[3]))
+            {
+                System.out.format("\n[%d, %d, %d]%d violates the SP bound", i[0], i[1], i[2], i[3]);
+            }
+        }
+    }
+
+    private void optionTen(Scanner reader) throws IOException
+    {
+        System.out.println("For the parity check matrix: ");
+        int[][] PCM = promptInputMatrix(reader);
+        int[][] vectors = promptInputVectors(reader);
+        int[][] cosetLeaders = CodeUtility.findCosetLeaders(PCM);
+
+        for (int i = 0; i < vectors.length; i++)
+        {
+            if (MatrixUtility.determineIfVectorsAreEqual(vectors[i], cosetLeaders[i]))
+            {
+                MatrixUtility.printVector(vectors[i]);
+                System.out.println("can be a coset leader");
+            }
+        }
+    }
+
+    private void optionEleven(Scanner reader) throws IOException
+    {
+        System.out.println("For the parity check matrix");
+        int[][] PCM = promptInputMatrix(reader);
+
+        System.out.println("What is the single bit error probability?");
+        double p = reader.nextDouble();
+
+        double result = CodeUtility.calculateProbabilityOfCorrectSyndromeDecoding(PCM, p);
+        System.out.println("Result: " + result);
+    }
+
+    private int[][] promptInputNKDParameters(Scanner reader) throws IOException
+    {
+        System.out.println("How many [n, k, d] parameter sets are there?");
+        int numberOfParameters = reader.nextInt();
+
+        int[][] parameters = new int[numberOfParameters][3];
+
+        for (int i = 0; i < numberOfParameters; i++)
+        {
+            System.out.println((i + 1) + ".) n = ? : ");
+            parameters[i][0] = reader.nextInt();
+
+            System.out.println((i + 1) + ".) k = ? : ");
+            parameters[i][1] = reader.nextInt();
+
+            System.out.println((i + 1) + ".) d = ? : ");
+            parameters[i][2] = reader.nextInt();
+        }
+
+        return parameters;
+    }
+
+    private int[][] promptInputNKDQParameters(Scanner reader) throws IOException
+    {
+        System.out.println("How many [n, k, d]q parameter sets are there?");
+        int numberOfParameters = reader.nextInt();
+
+        int[][] parameters = new int[numberOfParameters][4];
+
+        for (int i = 0; i < numberOfParameters; i++)
+        {
+            System.out.println((i + 1) + ".) n = ? ");
+            parameters[i][0] = reader.nextInt();
+
+            System.out.println((i + 1) + ".) k = ? ");
+            parameters[i][1] = reader.nextInt();
+
+            System.out.println((i + 1) + ".) d = ? ");
+            parameters[i][2] = reader.nextInt();
+
+            System.out.println((i + 1) + ".) q = ? ");
+            parameters[i][3] = reader.nextInt();
+        }
+
+        return parameters;
+    }
+
+    private int[][] promptInputVectors(Scanner reader) throws IOException
+    {
+        System.out.println("How many vectors are there?");
+        int rows = reader.nextInt();
+
+        System.out.println("What is the length of each vector?");
+        int columns = reader.nextInt();
+
+        int[][] vectors = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                System.out.println((i + 1) + ".) Value at index[" + j + "]? ");
+                vectors[i][j] = reader.nextInt();
+            }
+        }
+
+        return vectors;
+    }
+
+    private int[][] promptInputMatrix(Scanner reader) throws IOException
     {
         System.out.println("How many rows are in the matrix?: ");
-        int rows = reader.read();
+        int rows = reader.nextInt();
 
         System.out.println("How many columns are in the matrix?: ");
-        int columns = reader.read();
+        int columns = reader.nextInt();
 
         int[][] matrix = new int[rows][columns];
 
@@ -64,7 +296,7 @@ public class Main {
             for (int j = 0; j < columns; j++)
             {
                 System.out.println("Value at entry[" + i + "][" + j + "]?: ");
-                matrix[i][j] = reader.read();
+                matrix[i][j] = reader.nextInt();
             }
         }
 
--- src/MatrixUtility.java
+++ src/MatrixUtility.java
@@ -22,7 +22,33 @@ public class MatrixUtility {
 
     public static void printMatrix(int[][] matrix)
     {
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void printVector(int[] vector)
+    {
+        System.out.println();
+        for (int i = 0 ; i < vector.length; i++)
+        {
+            System.out.print(vector[i] + " ");
+        }
+    }
+
+    public static boolean determineIfVectorsAreEqual(int[] a, int[] b)
+    {
+        for (int i = 0; i < a.length; i++)
+        {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
     }
 
 }
